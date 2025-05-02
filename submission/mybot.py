13f7d27268f1a016deea7b1bd43b5cb6d7953bd0
Karkar15 <singingkd6@gmail.com>
2025-05-02 23:10:36 +1000

    comment

--- submission/mybot.py
+++ submission/mybot.py
@@ -1,9 +1,6 @@
 import random
 from pypokerengine.players import BasePokerPlayer
 
-# Comment - by Dhruv
-# Comment - by Karlyn
-# ANOTHER COMMENT
 
 # Notes
 # All cards follow this format: Suit + Rank : 4 of Hearts = 4H, 10 of Spades = ST [2,3,4,5,6,7,8,9,T,J,Q,K,A] [S,C,D,H]
